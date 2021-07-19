384b492b4efa42805cef6a7ba87d16cee21a1efc
Kevin Borrill <kevin.borrill@3sidedcube.com>
2021-07-19 09:14:50 +0100

    Add shrink on scroll

--- src/components/ui/Nav/index.tsx
+++ src/components/ui/Nav/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import classnames from "classnames";
 import variants from "constants/variants";
 
@@ -10,8 +10,31 @@ interface Props {
 };
 
 const Nav = ({ children, className = "", variant = variants.default, ...rest }: Props) => {
+  const [lastKnownScrollPosition, setLastKnownScrollPosition] = useState<number>(0);
+
+  useEffect(() => {
+    let ticking = false;
+
+    const callback = (e: Event) => {
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+          setLastKnownScrollPosition(window.scrollY);
+          ticking = false;
+        });
+
+        ticking = true;
+      }
+    }
+
+    document.addEventListener('scroll', callback);
+
+    return () => {
+      document.removeEventListener('scroll', callback);
+    }
+  }, []);
+
   return (
-    <nav className={classnames("c-nav", className)} {...rest}>
+    <nav className={classnames("c-nav", lastKnownScrollPosition > 50 && "c-nav--scrolled", className)} {...rest}>
       <div className="c-nav__inner">
         <h1>{process.env.REACT_APP_APP_NAME}</h1>
         {children}
